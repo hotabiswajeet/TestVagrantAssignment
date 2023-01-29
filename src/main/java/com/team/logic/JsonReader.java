92516ab13eca8dfa5865663a6617caefc4bb88eb
hotabiswajeet <hotabiswajeet333@gmail.com>
2023-01-29 13:45:04 +0530

    Added TestNG xml file and WicketKeeper Testcase

--- src/main/java/com/team/logic/JsonReader.java
+++ src/main/java/com/team/logic/JsonReader.java
@@ -5,27 +5,32 @@ import com.team.pojo.Player;
 import com.team.pojo.TeamRCB;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class JsonReader {
 
-    public ArrayList<Player> readJson() throws IOException {
+    public ArrayList<Player> readJson() throws IOException
+    {
         ObjectMapper mapper = new ObjectMapper();
         TeamRCB rcb = mapper.readValue(new File("src/main/team.json"), TeamRCB.class);
-        System.out.println(rcb.getLocation());
         ArrayList<Player> pl = rcb.getPlayer();
         return pl;
 
     }
 
-    public Long actualForeignPlayersNumbers(ArrayList<Player> pl)
+    public Long actualForeignPlayersInTeamCount(ArrayList<Player> pl)
     {
        Long count =  pl.stream().filter(s->!s.getCountry().equals("India")).count();
        return count;
+
+    }
+
+    public Long actualWicketKeeperInTeamCount(ArrayList<Player> pl)
+    {
+
+        Long count = pl.stream().filter(s->s.getRole().equalsIgnoreCase("Wicket-keeper")).count();
+        return count;
 
     }
 
--- src/test/java/com/team/test/TestCases.java
+++ src/test/java/com/team/test/TestCases.java
@@ -17,23 +17,44 @@ public class TestCases {
     JsonReader read;
 
     @BeforeTest
-    public void configFileReader() throws IOException {
+    public void configFileReader() throws IOException
+    {
         prop = new Properties();
         FileInputStream fis = new FileInputStream("src/main/java/com/team/config/configuration.properties");
         prop.load(fis);
+        read = new JsonReader();
 
     }
     @Test
-    public void foreignPlayersValidationTest() throws IOException {
-       read = new JsonReader();
-       ArrayList<Player> pl = read.readJson();
-       Long actualCount = read.actualForeignPlayersNumbers(pl);
-       System.out.println(actualCount);
-       Long expectedCount = Long.valueOf(prop.getProperty("AllowedForeignPlayers"));
-       if(actualCount<=expectedCount)
+    public void foreignPlayersValidationTest() throws IOException
+    {
+        ArrayList<Player> pl = read.readJson();
+       Long actualForeignPlayerCount = read.actualForeignPlayersInTeamCount(pl);
+       Long maxForeignPlayerCount = Long.valueOf(prop.getProperty("AllowedForeignPlayers"));
+       if(actualForeignPlayerCount<=maxForeignPlayerCount)
        {
            Assert.assertTrue(true,"Condition Matched");
        }
-       else {Assert.fail("Foreign Player condition not met, Expected count is "+prop.getProperty("AllowedForeignPlayers")+"");}
+       else
+       {
+           Assert.fail("Foreign Player condition not met, Allowed count is "+maxForeignPlayerCount+" where as actual count is "+actualForeignPlayerCount+"");
+       }
+    }
+
+    @Test
+    public void wicketKeeperValidationTest() throws IOException
+    {
+        ArrayList<Player> pl = read.readJson();
+        Long actualWicketKeeperCount = read.actualWicketKeeperInTeamCount(pl);
+        Long minimumWicketKeeperCount = Long.valueOf(prop.getProperty("MinimumWicketKeeperCount"));
+        if(actualWicketKeeperCount>=minimumWicketKeeperCount)
+        {
+            Assert.assertTrue(true,"Condition Matched");
+        }
+        else
+        {
+            Assert.fail("WicketKeeper condition not met, Minimum count is "+minimumWicketKeeperCount+" where as actual count is "+actualWicketKeeperCount+"");
+        }
+
     }
 }
